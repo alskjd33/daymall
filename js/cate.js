1e4df8a876e7814400f3f7ff6ace52608be6ed22
alskjd33 <alskjd33@gmail.com>
2024-08-01 17:23:03 +0900

    카테고리 스와이퍼적용

---
+++ js/cate.js
@@ -0,0 +1,47 @@
+window.addEventListener("load",function(){
+    const swCateall = new Swiper(".sw-cateall", {
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 51,
+        navigation: {
+            nextEl: ".cateall-slide .bt-next",
+            prevEl: ".cateall-slide .bt-prev",
+        },
+    } )
+    const swCateFood = new Swiper(".sw-catefood", {
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 51,
+        navigation: {
+            nextEl: ".catefood-slide .bt-next",
+            prevEl: ".catefood-slide .bt-prev",
+        },
+    } )
+    const swCateLife = new Swiper(".sw-catelife", {
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 51,
+        navigation: {
+            nextEl: ".catelife-slide .bt-next",
+            prevEl: ".catelife-slide .bt-prev",
+        },
+    } )
+    const swCateKitchen = new Swiper(".sw-catekitchen", {
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 51,
+        navigation: {
+            nextEl: ".catekitchen-slide .bt-next",
+            prevEl: ".catekitchen-slide .bt-prev",
+        },
+    } )
+    const swCateDog = new Swiper(".sw-catedog", {
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 51,
+        navigation: {
+            nextEl: ".catedog-slide .bt-next",
+            prevEl: ".catedog-slide .bt-prev",
+        },
+    } )
+})
