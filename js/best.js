4e6e935aa8bb4bee773b1b21adba916b7482bbb8
gwanyoung0822 <gwanyoung0822@gmail.com>
2024-08-02 13:17:32 +0900

    1280반응형

--- js/best.js
+++ js/best.js
@@ -2,7 +2,17 @@ window.addEventListener("load",function(){
     const swBest = new Swiper(".sw-best",{
         loop:true,
         slidesPerView: 4,
-        spaceBetween: 51,
+        spaceBetween: 60,
+        breakpoint:{
+            1025:{
+                slidesPerView: 4,
+                spaceBetween: 60,
+            },
+            1281:{
+                slidesPerView: 4,
+                spaceBetween: 51,
+            }
+        },
         navigation: {
             nextEl: ".best .bt-next",
             prevEl: ".best .bt-prev",
