ff0788abacf947b5df9fe0705dddeaf55f8be8ce
alskjd33 <alskjd33@gmail.com>
2024-08-06 18:06:59 +0900

    해더 768 320 카테고리  설정 배너 이미지수정

--- js/header.js
+++ js/header.js
@@ -1,15 +1,19 @@
-window.addEventListener("load",function(){
-    const hCate1List = this.document.querySelectorAll(".hCate1List") 
-    hCate1List.forEach(function(cate1){
-        // console.log(cate1);
-        cate1.addEventListener("click",function(){
-            hCate1List.forEach(function (i) {
-                // console.log(i);
-                if (i !== cate1) {
-                  i.classList.remove("active");
-                }
-              });
-              cate1.classList.toggle("active");
-        })
-    })
-})
+window.addEventListener("load", function () {
+  const hCate1List = this.document.querySelectorAll(".hCate1List");
+  hCate1List.forEach(function (cate1) {
+    // console.log(cate1);
+    cate1.addEventListener("click", function () {
+      hCate1List.forEach(function (i) {
+        // console.log(i);
+        if (i !== cate1) {
+          i.classList.remove("active");
+        }
+      });
+      cate1.classList.toggle("active");
+    });
+  });
+  //cate768 클릭시 나오기
+
+  // 클릭시 서치바 나오기
+
+});
