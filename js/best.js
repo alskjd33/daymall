cdf40f28f4f035ce3db0bda951668c489678bd4c
gwanyoung0822 <gwanyoung0822@gmail.com>
2024-08-07 17:23:08 +0900

    반응형

--- js/best.js
+++ js/best.js
@@ -1,9 +1,33 @@
 window.addEventListener("load",function(){
     const swBest = new Swiper(".sw-best",{
         loop:true,
-        slidesPerView: 3,
-        // spaceBetween: 60,
+        slidesPerView: 2.5,
+        spaceBetween: 30,
         breakpoints:{
+            321:{
+                slidesPerView:2,
+            },
+            350:{
+                slidesPerView:2.2,
+            },
+            400:{
+                slidesPerView:2.5,
+            },
+            470:{
+                slidesPerView:3,
+            },
+            481:{
+                slidesPerView:2.1,
+            },
+            520:{
+                slidesPerView:2.5,
+            },
+            570:{
+                slidesPerView:2.8,
+            },
+            630:{
+                slidesPerView:3,
+            },
             768:{
                 slidesPerView: 3,
                 spaceBetween: 54,
--- js/oneday.js
+++ js/oneday.js
@@ -1,9 +1,33 @@
 window.addEventListener("load",function(){
     const swOneday = new Swiper(".sw-oneday", {
         loop:true,
-        slidesPerView: 3,
-        // spaceBetween: 51,
+        slidesPerView: 2.5,
+        spaceBetween: 30,
         breakpoints:{
+            321:{
+                slidesPerView:2,
+            },
+            350:{
+                slidesPerView:2.2,
+            },
+            400:{
+                slidesPerView:2.5,
+            },
+            470:{
+                slidesPerView:3,
+            },
+            481:{
+                slidesPerView:2.1,
+            },
+            520:{
+                slidesPerView:2.5,
+            },
+            570:{
+                slidesPerView:2.8,
+            },
+            630:{
+                slidesPerView:3,
+            },
             768:{
                 slidesPerView: 3,
                 spaceBetween: 54,
