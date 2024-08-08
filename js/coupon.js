4f84864b3b215d87633c9be3f0755091714658b5
gwanyoung0822 <gwanyoung0822@gmail.com>
2024-08-08 17:08:12 +0900

    320 qr제거 쿠폰 보이는 갯수 조정

--- js/coupon.js
+++ js/coupon.js
@@ -1,14 +1,17 @@
 window.addEventListener("load",function(){
     const swCoupon = new Swiper(".sw-coupon",{
-        slidesPerView: 2.2,
+        slidesPerView: 2,
         breakpoints:{
             320:{
-                slidesPerView: 3.5
+                slidesPerView: 2.2
             },
             768:{
-                slidesPerView: 4,
+                slidesPerView: 3.5,
                 // spaceBetween: 60,
             },
+            1280:{
+                slidesPerView:4
+            }
         },
         navigation: {
             nextEl: ".coupon .bt-next",
